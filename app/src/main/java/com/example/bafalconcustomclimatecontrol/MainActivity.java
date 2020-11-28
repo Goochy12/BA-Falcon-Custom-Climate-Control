f4d488da918a71097e1cc53d9b578f2a80d58cb9
Goochy12-Laptop <liamgooch@live.com.au>
2020-11-28 23:38:50 +1100

    add button click variables

--- app/src/main/java/com/example/bafalconcustomclimatecontrol/MainActivity.java
+++ app/src/main/java/com/example/bafalconcustomclimatecontrol/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
             button_fanDown, button_tempUp, button_tempDown, button_domeLight, button_doorLock;
 
     private Button button_ac, button_acMax;
+
+    //boolean variables for button
+    private Boolean button_frontDefrost_isPressed, button_rearDefrost_isPressed, button_recycle_isPressed,
+    button_domeLight_isPressed, button_doorLock_isPressed, button_ac_isPressed, button_acMax_isPressed;
 
     private ProgressBar fanProgressBar, tempProgressBar;
 
@@ -41,11 +47,28 @@ public class MainActivity extends AppCompatActivity {
         fanProgressBar = (ProgressBar) findViewById(R.id.fanProgressBar);
         tempProgressBar = (ProgressBar) findViewById(R.id.tempProgressBar);
 
+        //set boolean variables
+        button_frontDefrost_isPressed = false;
+        button_rearDefrost_isPressed = false;
+        button_recycle_isPressed = false;
+        button_domeLight_isPressed = false;
+        button_doorLock_isPressed = false;
+        button_ac_isPressed = false;
+        button_acMax_isPressed = false;
+
         //declare button listeners
         button_ac.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(!button_ac_isPressed){
+                Log.i("debug_liam", "Working");
+                    button_ac.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+                    button_ac_isPressed = true;
+                }else{
+                    Log.i("debug_liam", "Working");
+                    button_ac.setBackgroundColor(getResources().getColor(R.color.black));
+                    button_ac_isPressed = false;
+                }
             }
         });
 
