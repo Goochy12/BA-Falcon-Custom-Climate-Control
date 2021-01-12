1fb27b1db4cda130a6a67111523db3fec0eb9aa1
Goochy12 <liamgooch@live.com.au>
2021-01-12 23:23:33 +1100

    Create UsbSerial.java

---
+++ app/src/main/java/com/liamgooch/bafalconcustomclimatecontrol/UsbSerial.java
@@ -0,0 +1,7 @@
+package com.liamgooch.bafalconcustomclimatecontrol;
+
+public class UsbSerial {
+    private static final String TAG = "BAFalcon-Test";
+    private static final String ACTION_USB_PERMISSION = "com.liamgooch.bafalconcustomclimatecontrol.permission";
+    public static final int ARDUINO_VENDOR_ID = 0x2341;
+}
