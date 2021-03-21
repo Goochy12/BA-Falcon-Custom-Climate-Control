5f9aa9ee18ee3abf016b482beb8fbba8546a6240
Goochy12-Laptop <liamgooch@live.com.au>
2021-03-21 19:22:54 +1100

    Update MainActivity.java

--- Android App/app/src/main/java/com/liamgooch/bafalconcustomclimatecontrol/MainActivity.java
+++ Android App/app/src/main/java/com/liamgooch/bafalconcustomclimatecontrol/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //TODO: refactor to have main serial declarations and management in the main activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
